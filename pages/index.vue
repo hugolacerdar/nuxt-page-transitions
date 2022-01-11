24d6d4695a2a7965eae6e5c3ed023002e80bd94a
Hugo <hugolacerdar@gmail.com>
2022-01-11 16:48:31 -0300

    Complete

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <h1>First Page</h1>
+    <ul>
+      <li><nuxt-link exact to="/second-page">Go to Second Page</nuxt-link></li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+  transition: "fadeOpacity",
+};
+</script>
+
+<style scoped>
+main {
+  background-color: #333;
+  color: white;
+  height: 100vh;
+  padding: 100px;
+}
+li > a {
+  color: white;
+}
+
+.fadeOpacity-enter-active,
+.fadeOpacity-leave-active {
+  transition: background-color 0.35s ease-out;
+}
+
+.fadeOpacity-enter-from,
+.fadeOpacity-leave-to {
+  background-color: rgb(87, 37, 37);
+}
+</style>
+
---
+++ pages/second-page.vue
@@ -0,0 +1,36 @@
+<template>
+  <main>
+    <h1>Second Page</h1>
+    <ul>
+      <li><nuxt-link exact to="/">Go to Index Page</nuxt-link></li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "SecondPage",
+};
+</script>
+
+<style scoped>
+main {
+  background-color: rgb(87, 37, 37);
+  color: white;
+  height: 100vh;
+  padding: 100px;
+}
+li > a {
+  color: white;
+}
+
+.fadeOpacity-enter-active,
+.fadeOpacity-leave-active {
+  transition: background-color 0.35s ease-out;
+}
+
+.fadeOpacity-enter-from,
+.fadeOpacity-leave-to {
+  background-color: #333;
+}
+</style>
